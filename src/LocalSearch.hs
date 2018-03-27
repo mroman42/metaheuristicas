e3610f43f01fdf28e2c99e117fcb868c93cfc5de
M42 <mromang08@gmail.com>
2018-03-27 23:44:42 +0200

    Cambios de rendimiento

--- src/LocalSearch.hs
+++ src/LocalSearch.hs
@@ -4,8 +4,12 @@ module LocalSearch where
 import Data.Random.Normal
 import System.Random
 import System.Random.Shuffle
+import System.Environment
+import Control.DeepSeq
+import Data.Time
+--import Data.Sequence as S
 
-import TemplateMain
+import Input
 import Base (nAttr)
 import LeaveOneOut
 
@@ -13,13 +17,14 @@ type Class = Double
 type Value = Double
 type Weight = Double
 type UnWeight = Double
-type Solution = [Weight]
+type Solution = [Weight] --S.Seq Weight
 type Instance = ([Value],Class)
 type Problem = [Instance]
 type Seed = Int
 
 
 -- Determina la desviación estándar de las distribuciones normales.
+{-# INLINE sigma #-}
 sigma :: Double
 sigma = 0.3
 
@@ -46,6 +51,7 @@ data Environment = Environment
   , gen :: StdGen
   , permutation :: [Int]  
   , solution :: Solution
+  , fitness :: !Double
   }
 
 -- Da un paso de exploración, que puede ser exitoso y llevar a una
@@ -54,63 +60,47 @@ data Environment = Environment
 -- contadores
 explore :: Problem -> Environment -> Environment
 
-explore training (Environment gStp lStp g [] w) =
-  explore training (Environment gStp lStp g (shuffle' [0..length w-1] (length w) g) w)
+explore training (Environment gStp lStp g [] w f) =
+  explore training (Environment gStp lStp g (shuffle' [0..length w-1] (length w) g) w f)
   
-explore training (Environment gStp lStp g (i:p) w)
-  | objective training w < objective training w' = Environment (gStp+1) 0        g' [] w'
-  | otherwise                                    = Environment (gStp+1) (lStp+1) g' p  w
+explore training (Environment gStp lStp g (i:p) w f)
+  | f < f'    = Environment (gStp+1) 0        g' [] w' f'
+  | otherwise = Environment (gStp+1) (lStp+1) g' p  w  f
   where
     w' = mutation epsilon i w :: Solution
+    f' = objective training w' :: Double
     (epsilon, g') = normal' (0,sigma) g 
 
  
 -- Explora hasta alcanzar cualquiera de las dos cotas.
-localSearch' :: Seed -> Problem -> Solution
-localSearch' s training = solution $ until
+localSearch :: Seed -> Problem -> Solution
+localSearch s !training = solution $ until
   (\env -> globalSteps env == 15000 || localSteps env == 20 * nAttr training)
   (explore training)
   initialEnvironment
   where
     initialEnvironment :: Environment
-    initialEnvironment = Environment 0 0 (mkStdGen s) [] initialSolution
+    initialEnvironment = Environment 0 0 (mkStdGen s) [] initialSolution (objective training initialSolution)
     initialSolution :: Solution
     initialSolution = take (nAttr training) $ randomRs (0.5,0.5) (mkStdGen s)
 
 
+templateMain :: (Seed -> Problem -> Solution) -> IO ()
+templateMain alg = do
+  -- Input
+  seed <- read . Prelude.head <$> getArgs
+  training <- string2ProblemNorm <$> getContents
+
+  -- Timing
+  start <- getCurrentTime
+  let sltn = alg seed training
+  stop <- sltn `deepseq` getCurrentTime
+  let diffTime = diffUTCTime stop start
+
+  -- Output
+  putStrLn $ "@time: " Prelude.++ show diffTime Prelude.++ "\n" Prelude.++ showSolution sltn
+
+
 
 main :: IO ()
-main = templateMain localSearch'
-
--- Devuelve una mejora local después de buscarla durante un tiempo.
--- localsearchstep :: (RandomGen g) => Int -> g -> Problem -> Solution -> Maybe Solution
--- localsearchstep searchBound gen training w =
---   -- Busca una mejora.
---   find (\w' -> objective training w < objective training w')
---   -- Limitada entre 20*n primeras variaciones que encuentre o entre la
---   -- cota global.
---   $ take (min searchBound (20 * length w))
---   -- Mutando a cada paso.
---   $ map (\(d,i) -> mutation d i w)
---   -- Según una distribución normal aplicada en cada paso a una
---   -- característica, elegida por una permutación de características.
---   $ zip
---     (normals' (0,sigma) gen)
---     (shuffle' (map (`mod` length w) [0..(20 * length w - 1)]) (20 * length w) gen)
-    
-
--- localsearch :: (RandomGen g) => g -> Problem -> Solution -> Solution
--- localsearch gen training = acc gen 15000
---   where
---     acc :: (RandomGen g) => g -> Int -> Solution -> Solution
---     acc _    0 s = s
---     acc gen' n s = case localsearchstep n gen' training s of
---                      Just s' -> acc ((snd . next) gen') (n-1) s'
---                      Nothing -> s
-
--- localSearch :: Seed -> Problem -> Solution
--- localSearch seed p = localsearch (mkStdGen seed) p initialSolution
---   where
---     -- La solución inicial se obtiene de una distribución uniforme.
---     initialSolution :: Solution
---     initialSolution = take (nAttr p) $ randomRs (0.5,0.5) (mkStdGen seed)
+main = templateMain localSearch
